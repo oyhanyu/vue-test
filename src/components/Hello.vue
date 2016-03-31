f9e01141a03545ce4ece38ae7889b9949ae10f4b
高保国 <gaobaoguo@360.cn>
2016-03-31 14:33:47 +0800

    新增文件

---
+++ src/components/Hello.vue
@@ -0,0 +1,27 @@
+<style lang="less">
+    .hello {
+      h1 {
+        color : red;
+      }
+    }
+</style>
+
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
